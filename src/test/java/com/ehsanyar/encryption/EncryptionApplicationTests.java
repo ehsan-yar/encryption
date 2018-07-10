0ceb68428431b12a12a7d7d6e84717763a1e53ea
ehsanyar1991 <ehsanyar1991@gmail.com>
2018-07-10 17:26:58 +0430

    add test

--- src/test/java/com/ehsanyar/encryption/EncryptionApplicationTests.java
+++ src/test/java/com/ehsanyar/encryption/EncryptionApplicationTests.java
@@ -1,7 +1,10 @@
 package com.ehsanyar.encryption;
 
+import com.ehsanyar.encryption.service.RSAEncryption;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EncryptionApplicationTests {
 
+	@Autowired
+	private RSAEncryption rsaEncryption;
+
+
+
 	@Test
 	public void contextLoads() {
+
 	}
 
+	@Test
+	public void RSATest(){
+
+		String message = "Ehsan Yar 1991";
+		System.out.println("Plain Message: " + message);
+
+		String publicKeyAsBase64 = rsaEncryption.getPublicKeyAsBase64();
+		System.out.println("PublicKey: " + publicKeyAsBase64);
+
+		String encryptedMessage = rsaEncryption.encryptAsString(message, publicKeyAsBase64);
+		System.out.println("EncryptedMessage: " + encryptedMessage);
+
+
+		String decryptMessage = rsaEncryption.decrypt(encryptedMessage);
+		System.out.println("DecryptMessage: " + decryptMessage);
+
+		Assert.assertEquals(message,decryptMessage);
+
+	}
 }
