b39b05e85aef4bf0c82817a1b97ab1f1bb29baae
ehsanyar1991 <ehsanyar1991@gmail.com>
2018-07-10 10:24:58 +0430

    add des, 3des, aes of dukpt test methods

--- src/test/java/com/ehsanyar/encryption/EncryptionApplicationTests.java
+++ src/test/java/com/ehsanyar/encryption/EncryptionApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.UnsupportedEncodingException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EncryptionApplicationTests {
@@ -47,31 +49,64 @@ public class EncryptionApplicationTests {
 
 
     @Test
-    public void testAesEncrypt() throws Exception {
-        // Setup
-        String bdkHexString = "0123456789ABCDEFFEDCBA9876543210"; // ANSI Test Key
-        String ksnHexString = "FFFF9876543210E00008";
-        String payloadString = "My name is Ehsan Yar Mohammadi";
-
-        System.out.println("Payload: "+payloadString);
-
-        byte[] bdk = dukpt.toByteArray(bdkHexString);
-        byte[] ksn = dukpt.toByteArray(ksnHexString);
-
-        byte[] encryptedPayload;
-        byte[] decryptedPayload;
-
-        // Action
-        byte[] key = dukpt.computeKey(bdk, ksn);
-        encryptedPayload = dukpt.encryptAes(key, payloadString.getBytes("UTF-8"), true);
-        decryptedPayload = dukpt.decryptAes(key, encryptedPayload,true);
-
-        String dataOutput = new String(decryptedPayload, "UTF-8").trim();
-
-        System.out.println("Decrypted: " + dataOutput);
-
-        // Assert
-        Assert.assertEquals(payloadString, dataOutput);
+    public void testDukptWithAesEncrypt() throws Exception {
+		dukptTest("AES");
 
     }
+
+	@Test
+	public void testDukptWithDesEncrypt() throws Exception {
+		dukptTest("DES");
+
+	}
+
+	@Test
+	public void testDukptWith3DesEncrypt() throws Exception {
+		dukptTest("3DES");
+
+	}
+
+    private void dukptTest(String algorithm) throws Exception {
+
+		// Setup
+		String bdkHexString = "0123456789ABCDEFFEDCBA9876543210"; // ANSI Test Key
+		String ksnHexString = "FFFF9876543210E00008";
+		String payloadString = "My name is Ehsan Yar Mohammadi";
+
+		System.out.println("Payload: "+payloadString);
+
+		byte[] bdk = dukpt.toByteArray(bdkHexString);
+		byte[] ksn = dukpt.toByteArray(ksnHexString);
+
+		byte[] encryptedPayload;
+		byte[] decryptedPayload = null;
+
+		// Action
+		byte[] key = dukpt.computeKey(bdk, ksn);
+
+		if (algorithm.toLowerCase().equals("aes")) {
+			encryptedPayload = dukpt.encryptAes(key, payloadString.getBytes("UTF-8"), true);
+			decryptedPayload = dukpt.decryptAes(key, encryptedPayload, true);
+		}
+
+		if (algorithm.toLowerCase().equals("des")) {
+			encryptedPayload = dukpt.encryptDes(key, payloadString.getBytes("UTF-8"), true);
+			decryptedPayload = dukpt.decryptDes(key, encryptedPayload, true);
+		}
+
+		if (algorithm.toLowerCase().equals("3des")) {
+			encryptedPayload = dukpt.encryptTripleDes(key, payloadString.getBytes("UTF-8"), true);
+			decryptedPayload = dukpt.decryptTripleDes(key, encryptedPayload, true);
+		}
+
+		assert decryptedPayload != null;
+		String dataOutput = new String(decryptedPayload, "UTF-8").trim();
+
+		System.out.println("Decrypted: " + dataOutput);
+
+		// Assert
+		Assert.assertEquals(payloadString, dataOutput);
+
+
+	}
 }
